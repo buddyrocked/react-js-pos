93d1fdde018780b4a86eda1561817adc06e3e83d
budi <bhariyana@kemana.com>
2019-05-16 15:53:39 +0700

    login

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { login } from '../../actions';
@@ -7,23 +8,87 @@ import { login } from '../../actions';
 class Login extends Component {
 
 	constructor(props) {
-		super(props);
-		this.state = {
-			showAlert : false,
-			route     : 'Login',
-			username  : 'superadmin',
-			password  : 'superadmin',
-			loading	  : false,
+    super(props);
+    this.state = {
+			showAlert: false,
+			route    : 'Login',
+			username : 'superadmin',
+			password : 'superadmin',
+			loading  : false,
 		};
 	};
 
-	render() {
-		return (
-			<div className="test">
-				Login Page
-			</div>
-		);
+	userLogin(e){
+		this.setState({
+			loading : true,
+		});
+
+		const values = JSON.stringify({
+			username : this.state.username,
+			password : this.state.password
+		});
+
+		this.props.onLogin(values, () => {
+		  	this.setState({
+		    	loading : false,
+		  	});
+		});
+
+		e.preventDefault();
+	}
+
+	showAlert = () => {
+		this.setState({
+		  	showAlert: true
+		});
+	};
+
+	hideAlert = () => {
+		this.setState({
+		  	showAlert: false
+		});
+	};
+
+  	
+
+  	render() {
+    	const { handleSubmit } = this.props;
+    	const {showAlert} = this.state;
+
+    	return (
+      		<div>
+        		<hr />
+        		<input
+        			type="text"
+                 	placeholder='Username'
+                 	value={this.state.username}
+                 	onChange={(text) => this.setState({ username: text })} />
+
+              	<input
+              		type="password"
+                  	placeholder='Password'
+                  	value={this.state.password}
+                  	onChange={(text) => this.setState({ password: text })} />
+
+              	<button
+                	onClick={ (e) => this.userLogin(e) } >
+                	SIGN IN
+              	</button>
+      		</div>
+    	);
+  	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		//token : state.auth.token
 	}
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onLogin: (values, callback) => { dispatch(login(values, callback)) },
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
